30785ddf35e20749fa12d5a136b689d2a9b3dad2
yashbharadwaj156 <62562859+yashbharadwaj156@users.noreply.github.com>
2021-08-23 10:58:11 +0530

    Add files via upload

---
+++ birthday.cpp
@@ -0,0 +1,83 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+#include<dos.h>
+int main()
+{
+	cout<<"\n\n\n\t\t\t\t\tHey you, there is something for u";
+	Sleep(2000);
+	Beep(500,500);
+	system("cls");
+	Sleep(10);
+	cout<<"\n\n\n\t\t\t\t\tIt's your day today my FRIEND";
+	Sleep(2000);
+	Beep(500,500);
+	system("cls");
+	cout<<"\n\n\n\t\t\t\t FROM:\tY A S H  B H A R A D W A J";
+	cout<<"\n\n\n\t\t\t\t TO:\tK R I S H N A  Y A D A V ";
+	cout<<"\n\t\t\t\t________________________________________________";
+	Sleep(2000);
+	Beep(700,500);
+	system("cls");
+	cout<<"\n\n\n\t\t\tH";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\tA";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\tP";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\tP";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\tY";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\t\tB";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\tI";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\tR";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\tT";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\tD";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\tA";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\tY";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\n\n\n\t\t\t\t\t     |||K R I S H N A    Y A D A V|||";
+	Sleep(100);
+	Beep(400,500);
+	cout<<"\n\n\n\t\t\t\t\t          ||1 8   A  P  R  I  L||";
+	Sleep(100);
+	Beep(1000,500);
+	cout<<"\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t|||I  WISH  YOU  A  VERY DYNAMIC  AND|||\n\n\n\t\t\t\t\t  ||  HAPPY  BIRTHDAY  MY FRIEND||";
+	cout<<"\n\n\n\t\t\t\t\t|YOU  ALWAYS  REAMINING  SMILING  AND  KEEP  GROWING  UP|";
+	Sleep(5000);
+	Beep(500,500);
+	system("cls");
+	cout<<"\n\n\n\t\t\t\t\t* PARTY IS STILL PENDING BRO\n\n\n\t\t\t\t\t P A R T Y\n\n\n";
+	
+	Sleep(5000);
+	Beep(500,500);
+
+	system("cls");
+	return 0;
+	
+	
+	
+	
+	
+
+}
